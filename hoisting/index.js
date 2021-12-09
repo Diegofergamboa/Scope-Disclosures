1d8d8a9f327bc2a88b57c27175931d5214e7ce38
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-09 18:58:02 -0500

    Update index.js

--- hoisting/index.js
+++ hoisting/index.js
@@ -4,4 +4,15 @@ var a ;
 // Asignación
 console.log(a) // 2
 
+console.log(b); // Undefined
+var b = 2;
+
+
+nameOfDog('Milúchis'); // Se puede correr por aquí
+
+function nameOfDog(name) {
+    console.log(name);
+}
+// Lo eleva a la memoria y luego, ejecuta la linea de código donde está
+// nameOfDog("Milú");
 
