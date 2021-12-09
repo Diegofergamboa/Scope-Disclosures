cdace1c48aa356ad93d48a93b3de7538de1d9e4c
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-09 15:16:24 -0500

    Update platzi.js

--- closures/platzi.js
+++ closures/platzi.js
@@ -1,10 +1,27 @@
-//! Esto no es un closure
+// //! Esto no es un closure
 
-const moneyBox = (coins) => {
+// const moneyBox = (coins) => {
+//     var saveCoins = 0;
+//     saveCoins += coins;
+//     console.log(saveCoins);
+// }
+
+// moneyBox(5);
+// moneyBox(10);
+
+
+const moneyBox = () => {
     var saveCoins = 0;
-    saveCoins += coins;
-    console.log(saveCoins);
+    const countCoins = (coins) => {
+        saveCoins += coins;
+        console.log(saveCoins);
+    }
+    return countCoins;
 }
 
+
+let money = moneyBox();
 moneyBox(5);
 moneyBox(10);
+
+console.log(money);
