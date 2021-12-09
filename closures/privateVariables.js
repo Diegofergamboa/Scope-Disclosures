176d9931c193b627bccd8b8f7543a3c17678b285
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-09 16:18:17 -0500

    Update privateVariables.js

--- closures/privateVariables.js
+++ closures/privateVariables.js
@@ -2,13 +2,38 @@
 
 
 const Person = () => {
-    var name = "Name";
+    var saveName = "Name"; // Basicamente la variable a la que no podremos acceder de manera ordinaria. porque es privada basicamente
         {
-            getName : (name) => {
-                console.log(name);
+            getName = (name) => {
+                return saveName;
             } , 
-            setName : (name => {
-                
-            })
-        }
-}
+            setName = (name) => {
+                saveName = name;
+            }
+        };
+};
+
+var newPerson = Person();
+// Agregar una nueva persona
+newPerson = setName('Diego');
+console.log(getName(newPerson));
+
+
+const Password = () => {
+    var password = 'passsword' // A esta variable nunca se podrá acceder si no es por medio de los métodos que establecimos.
+        {
+            changePassword : (newPassword) => {
+                password = newPassword ;
+            }
+            showPassword : (password) => {
+                return password;
+            }
+        };
+};
+
+var newPassword = Password();
+console.log(`La contraseña anterior es ${showPassword(newPassword)}`);
+newPassword = changePassword('Nueva contraseña.');
+console.log(`La contraseña anterior es ${showPassword(newPassword)}`);
+
+
