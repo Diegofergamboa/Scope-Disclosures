1c5b5664e8c4c36792981ec119c45eae5f84b433
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-10-22 16:45:29 -0500

    studying the scope of the block scope

---
+++ new-course/src/scope/block.js
@@ -0,0 +1,16 @@
+//! Funcion en bloque.
+//! Es una actualización de ECMAScript.
+
+function fruits() {
+    // Todo lo que tenga handle va a ser un bloque de código
+    if (true) {
+        var var1 = 1; // global scope
+        let var2 = 2; // block scope
+        const var3 = 3; // block scope
+    }
+    console.log(var1);
+    console.log(var2);
+    console.log(var3);
+}
+// No se debería usar las lógica de var para declarar variables porque como sobreescriben en el ámbito global, podrían hacer llamados que no se quieran en un futuro
+fruits();
