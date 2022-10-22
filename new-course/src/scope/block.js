5d4701d8e8c1fc3701bb02690778cd757ad62266
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-10-22 16:46:00 -0500

    Update block.js

--- new-course/src/scope/block.js
+++ new-course/src/scope/block.js
@@ -7,10 +7,11 @@ function fruits() {
         var var1 = 1; // global scope
         let var2 = 2; // block scope
         const var3 = 3; // block scope
+        console.log(var2);
+        console.log(var3);
     }
     console.log(var1);
-    console.log(var2);
-    console.log(var3);
+    
 }
 // No se debería usar las lógica de var para declarar variables porque como sobreescriben en el ámbito global, podrían hacer llamados que no se quieran en un futuro
 fruits();
