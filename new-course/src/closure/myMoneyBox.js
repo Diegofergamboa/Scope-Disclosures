ddb9e06b58f6a08da74af8b726fa059c1b4674fe
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-10-24 17:29:01 -0500

    Update myMoneyBox.js

--- new-course/src/closure/myMoneyBox.js
+++ new-course/src/closure/myMoneyBox.js
@@ -1,2 +1,24 @@
 //! Ejemplo de closures.
 
+function myMoneyBox() {
+    let saveCoins = 0;
+
+    function increaseCoins(coins) {
+        saveCoins += coins;
+        console.log(`Saved money is $${saveCoins}`);
+    };
+    return increaseCoins;
+};
+
+//! Otra instancia de la función de save Money para Diego.
+const myMoneyBoxDiego = myMoneyBox();
+//! Otra instancia de la función de save Money para Fernando.
+const myMoneyBoxFernando = myMoneyBox();
+
+myMoneyBoxDiego(10);
+myMoneyBoxDiego(10);
+myMoneyBoxDiego(20);
+console.log('Another count.')
+myMoneyBoxFernando(10);
+myMoneyBoxFernando(15);
+myMoneyBoxFernando(20);
