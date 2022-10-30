757e35455bb5f856382e9dbadd7c6fd56d530d5c
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-10-30 16:12:31 -0500

    Update index.js

--- new-course/src/hoisting/index.js
+++ new-course/src/hoisting/index.js
@@ -1,6 +1,24 @@
-function callA() {
-    console.log(a)
-}
+// function callA() {
+//     console.log(a)
+// }
 
-var a = 0;
-callA();
+// var a = 0;
+// callA();
+
+// nameOfDog("elmo");
+
+// function nameOfDog(name) {
+//     console.log(name);
+// }
+
+const fruits = () => {
+    if (true) { 
+        var fruit1 = 'apple';
+        const fruit2 = 'banana';
+        let fruit3 = 'kiwi'; 
+    }
+    console.log(fruit1);
+}
+fruits();
+
+
